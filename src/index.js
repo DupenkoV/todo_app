dbbb42fc677598d8ab6bc32f6cf4b0174a0d1dfb
Vladimir <Volor88@ya.ru>
2023-02-28 15:15:58 +0300

    added work with localstorage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { store } from "./store/index";
+import { configureStore } from "./store/index";
 import { Provider } from "react-redux";
 
 
@@ -8,6 +8,7 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const store = configureStore();
 
 root.render(
   <Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,28 @@ import { createStore } from "redux";
 import {combineReducers} from 'redux';
 import { filters } from './filters/filter-reducer'
 import { todoList } from "./todos/todos-reducer";
+import { loadState, saveState } from './local-storage'
 
 export const rootReducer = combineReducers({
     todoList,
     filters,
 })
 
-export const store = createStore(
+
+
+export const configureStore = () => {
+  const persistedState = loadState();
+
+  const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.subscribe(() => {
+    saveState({
+      todoList: store.getState().todoList,
+    })
+  })
+  return store;
+} 
